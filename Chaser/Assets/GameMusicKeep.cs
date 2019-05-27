d17bba2cd823bffac9f6952ce13b0b80a9b4e23d
Calvin Edwards <calvinl.edwards@gmail.com>
2019-05-26 19:18:56 -0700

    fixed flight and local rotation... waypoints still fucked upt

---
+++ Chaser/Assets/GameMusicKeep.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMusicKeep : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SceneLoader", 5.0f);
+    }
+
+    private void SceneLoader()
+    {
+        SceneManager.LoadScene(1);            
+    }
+}
